8d1bb4ddf8466738c9d24e86c5e450b7a436d737
Rafael Maldonado <RafaelJMaldonado@gmail.com>
2015-08-19 12:12:23 -0400

    Build reddit search tool.

---
+++ js/reddit-search.js
@@ -0,0 +1,27 @@
+var parseResults = function(results){
+  $(".search-results").empty();
+  var searchResults = results.data.children;
+  _.templateSettings.variable = "searchResults";
+
+  var template = _.template(
+          $( "script.template" ).html()
+  );
+
+  var templateData = searchResults;
+
+  $("#reddit-search-form").after(
+    template( templateData )
+  );
+};
+
+var bindListener = function(){
+  $("#reddit-search-form").submit(function(event){
+    event.preventDefault();
+    var input = $(event.currentTarget).find("#search-value").val();
+
+
+    $.getJSON( "http://www.reddit.com/search.json?q=" + input + "?type=link?sort=top", function( results ) {
+      parseResults(results);
+    });
+  });
+}
